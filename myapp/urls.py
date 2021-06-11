441a22866d77b3b4bd7dff93888be17dbac0b3cf
Anil_Banskota <anil.123aaaaaa@gmail.com>
2021-06-11 17:25:37 +0545

    finalize templates

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.helloworld, name="helloworld"),
-    path('', views.helloworld2, name="helloworld2"),
+    path('1', views.helloworld, name="helloworld"),
+    path('2', views.helloworld2, name="helloworld2"),
 ]
